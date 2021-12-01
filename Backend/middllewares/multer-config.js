9eb35e81c82e783b76e8c7166fdd52547bb186af
LKONATE <lkonate78@yahoo.com>
2021-12-01 18:59:07 +0100

    multer

--- Backend/middllewares/multer-config.js
+++ Backend/middllewares/multer-config.js
@@ -6,6 +6,7 @@ const MIME_TYPES = {
   'image/png': 'png'
 };
 
+//logique pour les stockage telechargement de fichier
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
