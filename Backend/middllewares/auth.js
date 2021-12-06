d836845960e91de88e32f81745607c5e6c159a1a
LKONATE <lkonate78@yahoo.com>
2021-12-06 19:40:21 +0100

    correction

--- Backend/middllewares/auth.js
+++ Backend/middllewares/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET_78190_44600');
     const userId = decodedToken.userId;
-   
+    req.auth = { userId };  
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
